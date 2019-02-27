7058a43f226ff30496f0ec23b35f0925c79ce192
Chrisxd205 <47583623+Chrisxd205@users.noreply.github.com>
2019-02-27 15:25:44 -0800

    segundo programa
    segundo programa de la tarea pero no supe como hacer la comparacion

---
+++ Dados/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Dados
+{
+    
+
+    class Dados
+    {
+public string color;
+public int valor;
+ public Dados(int valor)
+ {
+this.valor=valor;
+ }
+ public Dados(string color)
+ {
+  this.color=color;
+ }
+ public void imprimir()
+ {
+     Console.WriteLine(valor);
+ }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dados d1= new Dados(4);
+           Dados d2 = new Dados(3);
+           Dados d3 = new Dados(1);
+            d1.imprimir();
+            d2.imprimir();
+            d3.imprimir();
+       
+        }
+    }
+}
