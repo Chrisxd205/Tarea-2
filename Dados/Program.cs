a7bf7f89bdee7e4f8b23d526afacdc808e4e788d
Chrisxd205 <47583623+Chrisxd205@users.noreply.github.com>
2019-02-27 13:51:54 -0800

    programa this
    primera parte del programa this

---
+++ Dados/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dados
+{
+
+    class dados
+    {
+         public int valor;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ programthis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+class Employee
+{
+    private string name;
+    private string alias;
+   
+
+    
+    public Employee(string name, string alias)
+    {
+        
+        this.name = name;
+        this.alias = alias;
+    }
+
+   
+    public void printEmployee()
+    {
+        Console.WriteLine("Name: {0}\nAlias: {1}", name, alias);
+     
+    }
+
+
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+        {
+        Employee E1 = new Employee("Mingda Pan", "mpan");
+
+      
+        E1.printEmployee();
+    }
+}
